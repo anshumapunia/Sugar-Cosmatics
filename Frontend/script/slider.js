81a0fce209eeba7c728f0cc5a3feeac617208afd
anshumapunia123 <112813318+anshumapunia123@users.noreply.github.com>
2023-05-04 21:17:18 +0530

    project

---
+++ Frontend/script/slider.js
@@ -0,0 +1,24 @@
+function first(){
+    document.getElementById("slideimage").src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F8d883679-e974-460e-b478-64614da082da.jpg&w=1920&q=75"
+}
+function second(){
+    document.getElementById("slideimage").src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2Fc1c0fae0-f339-4378-b29f-e95b8c474e55.jpg&w=1920&q=75"
+}
+function third(){
+    document.getElementById("slideimage").src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2Fcd9aec57-0010-49f2-a903-47b581127743.gif&w=1920&q=75"
+}
+function fourth(){
+    document.getElementById("slideimage").src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F27024021-0638-4225-b6df-c7f48942bdf4.jpg&w=1920&q=75"
+}
+function fifth(){
+    document.getElementById("slideimage").src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F8d883679-e974-460e-b478-64614da082da.jpg&w=1920&q=75"
+}
+function sixth(){
+    document.getElementById("slideimage").src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F9723d8e3-9e99-459f-acfe-4ef93089e9ef.jpg&w=1920&q=75"
+}
+setInterval(first,2000);
+setInterval(second,4000);
+setInterval(third,6000);
+setInterval(fourth,8000);
+setInterval(fifth,9000);
+setInterval(sixth,10000);
